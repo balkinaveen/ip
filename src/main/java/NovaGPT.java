9f92197e79ac76bfaa0dfe785568621c417a7f29
balkinaveen <balkinaveen@gmail.com>
2025-08-22 14:42:50 +0800

    Rename, Greet, Exit

---
+++ src/main/java/NovaGPT.java
@@ -0,0 +1,12 @@
+public class NovaGPT {
+    public static void main(String[] args) {
+        System.out.println("_____________________________________________________");
+        System.out.println("Hello, I'm NovaGPT");
+        System.out.println("What can I do for you today?");
+        System.out.println("_____________________________________________________");
+        System.out.println("Bye. Hope to see you soon!");
+        System.out.println("HAND!");
+        System.out.println("_____________________________________________________");
+
+    }
+}
