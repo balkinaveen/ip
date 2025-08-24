138c629c9eb374c4d9d5003a0bb45e1b57bfbb35
balkinaveen <balkinaveen@gmail.com>
2025-08-25 04:05:07 +0800

    echo

--- src/main/java/NovaGPT.java
+++ src/main/java/NovaGPT.java
@@ -1,12 +1,31 @@
-public class NovaGPT {
-    public static void main(String[] args) {
-        System.out.println("_____________________________________________________");
-        System.out.println("Hello, I'm NovaGPT!");
-        System.out.println("What can I do for you today?");
-        System.out.println("_____________________________________________________");
-        System.out.println("Bye. Hope to see you soon!");
-        System.out.println("HAND!");
-        System.out.println("_____________________________________________________");
+import java.util.Scanner;
 
+public class NovaGPT {
+    private static final String welcomeMessage = "Hello, I'm NovaGPT!\nWhat can I do for you today?";
+    private static final String goodbyeMessage = "Bye. Hope to see you soon!\nHAND!";
+    private static final String horizontalLine = "____________________________________________________________";
+    private static final String killswitch = "bye";
+
+    public static void printer(String message) {
+        System.out.print(horizontalLine + "\n" + message + "\n" + horizontalLine + "\n");
+    }
+
+    public static void run() {
+        printer(welcomeMessage);
+        String input = "";
+        Scanner sc = new Scanner(System.in);
+
+        while(!input.toLowerCase().equals(killswitch)) {
+            input = sc.nextLine();
+            if (input.toLowerCase().equals(killswitch)) {
+                break;
+            }
+            printer(input);
+        }
+        printer(goodbyeMessage);
+    }
+
+    public static void main(String[] args) {
+        NovaGPT.run();
     }
 }
