f4aaa510d8fbe46d5af568fd81c6d4efb1d5ad77
balkinaveen <balkinaveen@gmail.com>
2025-09-02 11:29:09 +0800

    Make the code more OOP

---
+++ src/main/java/Ui.java
@@ -0,0 +1,8 @@
+public class Ui {
+
+    private static final String HORIZONTAL_LINE = "____________________________________________________________";
+
+    public static void print(String message) {
+        System.out.print(HORIZONTAL_LINE + "\n" + message + "\n" + HORIZONTAL_LINE + "\n");
+    }
+}
