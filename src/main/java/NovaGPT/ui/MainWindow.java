76e60fa60f33ed850a32a858bdeaccf73ee1426a
balkinaveen <balkinaveen@gmail.com>
2025-09-13 14:27:53 +0800

    Implement Ui for the chatbot

---
+++ src/main/java/NovaGPT/ui/MainWindow.java
@@ -0,0 +1,62 @@
+package novagpt.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+/**
+ * Controller for the main GUI.
+ */
+public class MainWindow extends AnchorPane {
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private TextField userInput;
+    @FXML
+    private Button sendButton;
+
+
+    private NovaGpt novagpt;
+
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaChatter.png"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaNova.png"));
+
+
+    /** Initialises the ui */
+    @FXML
+    public void initialize() {
+        scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+        dialogContainer.getChildren().add(DialogBox.getDukeDialog(Ui.welcomeMessage(), dukeImage));
+    }
+
+    /** Injects the Duke instance */
+    public void setNovagpt(NovaGpt n) {
+        assert n != null;
+        novagpt = n;
+    }
+
+    /**
+     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
+     * the dialog container. Clears the user input after processing.
+     */
+    @FXML
+    private void handleUserInput() {
+        String input = getUserInput();
+        String response = novagpt.response(input);
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(input, userImage),
+                DialogBox.getDukeDialog(response, dukeImage)
+        );
+        userInput.clear();
+    }
+
+    String getUserInput() {
+        return userInput.getText();
+    }
+
+}
--- src/main/java/NovaGPT/ui/NovaGPT.java
+++ src/main/java/NovaGPT/ui/NovaGPT.java
@@ -1,7 +1,6 @@
 package novagpt.ui;
 
 import java.util.ArrayList;
-import java.util.Scanner;
 
 import novagpt.command.Command;
 import novagpt.command.Parser;
@@ -21,77 +20,59 @@ import novagpt.task.Task;
  */
 
 public class NovaGpt {
-    /**
-     * Starts the main program loop.
-     * Continuously read for user input, parse the input into commands,
-     * executes the commands and its relevant actions and provide the output through the UI.
-     * Continues until the exit command is released through the input "bye".
-     *
-     * @param filePath This is the filePath in which the application stores its list of tasks.
-     */
-    public static void run(String filePath) {
-        Ui.welcomeMessage();
-        String input = "";
-        Scanner sc = new Scanner(System.in);
-        Storage st = new Storage(filePath);
-        ArrayList<Task> ls = st.load();
 
-        while (!input.toLowerCase().equals(Ui.KILL_SWITCH)) {
-            input = sc.nextLine();
-            Command command = Parser.parseCommandFromInput(input);
-            try {
-                switch (command) {
-                case BYE:
-                    break;
-                case LIST:
-                    TaskList.handleList(ls);
-                    break;
-                case MARK:
-                    TaskList.handleMark(input, ls, st);
-                    break;
-                case UNMARK:
-                    TaskList.handleUnMark(input, ls, st);
-                    break;
-                case TODO:
-                    TaskList.handleTodoTask(input, ls, st);
-                    break;
-                case DEADLINE:
-                    TaskList.handleDeadlineTask(input, ls, st);
-                    break;
-                case EVENT:
-                    TaskList.handleEventTask(input, ls, st);
-                    break;
-                case DELETE:
-                    TaskList.handleDelete(input, ls, st);
-                    break;
-                case FIND:
-                    TaskList.handleFind(input, ls, st);
-                    break;
-                case MAN:
-                    Ui.print(Ui.LIST_OF_COMMANDS);
-                    break;
-                case UNKNOWN:
-                    throw new NovaException("Hold up! I'm sorry! \n"
-                            + "I don't get what that means, please try again :-( \n"
-                            + "Prompt 'man' for help"
-                            );
-                default:
-                    throw new NovaException("Error, try again");
-                }
-            } catch (NovaException e) {
-                Ui.errorMessage(e.getMessage());
-            } catch (Exception e) {
-                Ui.unexpectedErrorMessage(e.getMessage());
-            }
-        }
-        Ui.goodbyeMessage();
+    private final Storage storage;
+    private final ArrayList<Task> tasks;
+
+    /**
+     * Initializes a chatbot instance
+     * @param filePath is where the file is stored
+     */
+    public NovaGpt(String filePath) {
+        this.storage = new Storage(filePath);
+        this.tasks = storage.load();
     }
-
     /**
-     * This is the main method which calls the run method.
-     * @param args Unused.
+     * Parses the input and gives the output
+     * @param input is the input provided by user
      */
-    public static void main(String[] args) {
-        NovaGpt.run("./data/NovaGPT.txt");
+    public String response(String input) {
+        Command command = Parser.parseCommandFromInput(input);
+        try {
+            switch (command) {
+            case BYE:
+                break;
+            case LIST:
+                return TaskList.handleList(tasks);
+            case MARK:
+                return TaskList.handleMark(input, tasks, storage);
+            case UNMARK:
+                return TaskList.handleUnMark(input, tasks, storage);
+            case TODO:
+                return TaskList.handleTodoTask(input, tasks, storage);
+            case DEADLINE:
+                return TaskList.handleDeadlineTask(input, tasks, storage);
+            case EVENT:
+                return TaskList.handleEventTask(input, tasks, storage);
+            case DELETE:
+                return TaskList.handleDelete(input, tasks, storage);
+            case FIND:
+                return TaskList.handleFind(input, tasks, storage);
+            case MAN:
+                return Ui.print(Ui.LIST_OF_COMMANDS);
+            case UNKNOWN:
+                throw new NovaException("Hold up! I'm sorry! \n"
+                        + "I don't get what that means, please try again :-( \n"
+                        + "Prompt 'man' for help"
+                        );
+            default:
+                throw new NovaException("Error, try again");
+            }
+        } catch (NovaException e) {
+            return Ui.errorMessage(e.getMessage());
+        } catch (Exception e) {
+            return Ui.unexpectedErrorMessage(e.getMessage());
+        }
+        return null;
     }
 }
